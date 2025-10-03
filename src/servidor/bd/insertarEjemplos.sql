443d30b5f3668adba2fe7f21461b66c8d5319c7a
Meryame Ait Boumlik <meryemboumlik612@gmail.com>
2025-10-03 15:53:34 +0200

    e

--- src/servidor/bd/insertarEjemplos.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO Mediciones (tipo, valor, instante) 
-VALUES ('CO2', 400, '2025-02-21T12:00:00');
-
-INSERT INTO Mediciones (tipo, valor, instante) 
-VALUES ('TEMPERATURA', 21, '2025-02-21T12:05:00');
-
-INSERT INTO Mediciones (tipo, valor, instante) 
-VALUES ('RUIDO', 35, '2025-02-21T12:10:00');
